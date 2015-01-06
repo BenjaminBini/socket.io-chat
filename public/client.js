17b4d868b4ca4cc2eb08cf845730eae845e962a1
BenjaminBini <benjamin.bini@gmail.com>
2015-01-06 12:09:56 +0100

    User list management and messages archive

--- public/client.js
+++ public/client.js
@@ -9,14 +9,15 @@ var socket = io();
  */
 function scrollToBottom() {
   if ($(window).scrollTop() + $(window).height() + 2 * $('#messages li').last().outerHeight() >= $(document).height()) {
-    $("html, body").animate({ scrollTop: $(document).height() }, 0);
+    $('html, body').animate({ scrollTop: $(document).height() }, 0);
   }
 }
 
 /*** Gestion des événements ***/
 
 /**
- * Connexion d'un utilisateur
+ * Connexion de l'utilisateur
+ * Uniquement si le username n'est pas vide et n'existe pas encore
  */
 $('#login form').submit(function (e) {
   e.preventDefault();
@@ -24,9 +25,12 @@ $('#login form').submit(function (e) {
     username : $('#login input').val().trim()
   };
   if (user.username.length > 0) { // Si le champ de connexion n'est pas vide
-    socket.emit('user-login', user);
-    $('body').removeAttr('id'); // Cache formulaire de connexion
-    $('#chat input').focus(); // Focus sur le champ du message
+    socket.emit('user-login', user, function (success) {
+      if (success) {
+        $('body').removeAttr('id'); // Cache formulaire de connexion
+        $('#chat input').focus(); // Focus sur le champ du message
+      }
+    });
   }
 });
 
@@ -59,4 +63,22 @@ socket.on('chat-message', function (message) {
 socket.on('service-message', function (message) {
   $('#messages').append($('<li class="' + message.type + '">').html('<span class="info">information</span> ' + message.text));
   scrollToBottom();
+});
+
+/**
+ * Connection d'un nouvel utilisateur
+ */
+socket.on('user-login', function (user) {
+  $('#users').append($('<li class="' + user.username + ' new">').html(user.username));
+  setTimeout(function () {
+    $('#users li.new').removeClass('new');
+  }, 1000);
+});
+
+/**
+ * Déconnection d'un utilisateur
+ */
+socket.on('user-logout', function (user) {
+  var selector = '#users li.' + user.username;
+  $(selector).remove();
 });
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var i;
 
 /**
  * Gestion des requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
  */
-app.use("/", express.static(__dirname + "/public"));
+app.use('/', express.static(__dirname + '/public'));
+
+/**
+ * List of connected users
+ */
+var users = [];
+
+/**
+ * Historique des messages
+ */
+var messages = [];
 
 io.on('connection', function (socket) {
 
@@ -21,7 +32,29 @@ io.on('connection', function (socket) {
   console.log('a user connected');
 
   /**
-   * Déconnexion d'un utilisateur : broadcast d'un 'service-message'
+   * Emission d'un événement "user-login" pour chaque utilisateur connecté
+   */
+  for (i = 0; i < users.length; i++) {
+    socket.emit('user-login', users[i]);
+  }
+
+  /** 
+   * Emission d'un événement "chat-message" pour chaque message de l'historique
+   */
+  for (i = 0; i < messages.length; i++) {
+    if (messages[i].username !== undefined) {
+      socket.emit('chat-message', messages[i]);
+    } else {
+      socket.emit('service-message', messages[i]);
+    }
+  }
+
+  /**
+   * Déconnexion d'un utilisateur :
+   *  - suppression de la liste des connectés
+   *  - broadcast d'un 'service-message'
+   *  - ajout du message à l'historique
+   *  - broadcast d'un 'user-logout' contenant le user
    */
   socket.on('disconnect', function () {
     if (loggedUser !== undefined) {
@@ -30,33 +63,66 @@ io.on('connection', function (socket) {
         text: 'User "' + loggedUser.username + '" disconnected',
         type: 'logout'
       };
+      var userIndex = users.indexOf(loggedUser);
+      if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+      }
       socket.broadcast.emit('service-message', serviceMessage);
+      messages.push(serviceMessage);
+      io.emit('user-logout', loggedUser);
     }
   });
 
   /**
    * Connexion d'un utilisateur via le formulaire :
    *  - sauvegarde du user
+   *  - ajout à la liste des connectés
+   *  - émission d'un 'service-message' pour rappeler le pseudo de l'utilisateur
    *  - broadcast d'un 'service-message'
+   *  - ajout du message broadcasté à l'historique
+   *  - événement global contenant le user
+   *  - appel du callback
    */
-  socket.on('user-login', function (user) {
-    loggedUser = user;
-    if (loggedUser !== undefined) {
-      var serviceMessage = {
+  socket.on('user-login', function (user, callback) {
+    var userIndex = -1;
+    for (i = 0; i < users.length; i++) {
+      if (users[i].username === user.username) {
+        userIndex = i;
+      }
+    }
+    if (user !== undefined && userIndex === -1) {
+      loggedUser = user;
+      users.push(loggedUser);
+      var userServiceMessage = {
+        text: 'You logged in as "' + loggedUser.username + '"',
+        type: 'login'
+      };
+      var broadcastedServiceMessage = {
         text: 'User "' + loggedUser.username + '" logged in',
         type: 'login'
       };
-      socket.broadcast.emit('service-message', serviceMessage);
+      socket.emit('service-message', userServiceMessage);
+      socket.broadcast.emit('service-message', broadcastedServiceMessage);
+      messages.push(broadcastedServiceMessage);
+      io.emit('user-login', loggedUser);
+      callback(true);
+    } else {
+      callback(false);
     }
   });
 
   /**
    * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
+   * Ajout à la liste des messages et purge si nécessaire
    */
   socket.on('chat-message', function (message) {
     message.username = loggedUser.username;
     io.emit('chat-message', message);
     console.log('Message de : ' + loggedUser.username);
+    messages.push(message);
+    if (messages.length > 150) {
+      messages.splice(0, 1);
+    }
   });
 });
 
